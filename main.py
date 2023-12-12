8957f79acd24c8c003d4b80ff1a77d9026728627
Jesse_A <146388635+mugeltun@users.noreply.github.com>
2023-12-11 20:00:22 -0500

    current weather report
    used the information from the zipcode to get current temp data
    also made variables for each data point i will provide the user

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ API_KEY = 'ce965a163d729883d7494b4b5fa5ac57'
 zip_code = input("please enter the zipcode of the location you wish to check the weather in: ")
 
 
-location_request = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},US&appid={API_KEY}')
-location_request_json = location_request.json()
-longitude = location_request_json['lat']
-latitude = location_request_json['lon']
+location = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},US&appid={API_KEY}')
+location_json = location.json()
+longitude = location_json['lon']
+latitude = location_json['lat']
 
-print(latitude,longitude)
+cur_weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=imperial')
+cur_weather_json = cur_weather.json()
+
+cur_temp = cur_weather_json['main']['temp']
+cur_feels_like = cur_weather_json['main']['feels_like']
+cur_temp_max = cur_weather_json['main']['temp_max']
+cur_temp_min = cur_weather_json['main']['temp_min']
+
+print(f"The location you chose is {location_json['name']}")
+print(cur_weather_json)
+print(f"this weather today will be {cur_temp} degrees farienheight and it feels like {cur_feels_like}")
