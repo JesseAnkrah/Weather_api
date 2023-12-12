0d62a3374c68f2f8437eb774e1c476e21b683040
Jesse_A <146388635+mugeltun@users.noreply.github.com>
2023-12-11 19:04:52 -0500

    Location for weather api
    used the zipcode to get the longitude and latitude for the current weather api

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ zip_code = input("please enter the zipcode of the location you wish to check the
 
 
 location_request = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},US&appid={API_KEY}')
-print(location_request.text)
+location_request_json = location_request.json()
+longitude = location_request_json['lat']
+latitude = location_request_json['lon']
+
+print(latitude,longitude)
