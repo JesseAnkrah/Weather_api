555d619febc0a3aab5ea649e454b942ff4cbea4b
Jesse_A <146388635+mugeltun@users.noreply.github.com>
2023-12-13 19:04:37 -0500

    Output information
    updated output information to contain the sky description, max temp,
    and feels like information

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ print("this is a weather checker for the united states please follow the directi
 def zip_code_func():
     location_zip = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},US&appid={API_KEY}')
     location_zip_json = location_zip.json()
+    
     longitude = location_zip_json['lon']
     latitude = location_zip_json['lat']
 
     cur_weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=imperial')
     cur_weather_json = cur_weather.json()
     
-    cur_temp = cur_weather_json['main']['temp']
     cur_feels_like = cur_weather_json['main']['feels_like']
     cur_temp_max = cur_weather_json['main']['temp_max']
-    cur_temp_min = cur_weather_json['main']['temp_min']
+    cur_sky = cur_weather_json['weather'][0]['description']
 
-    print(cur_weather_json)
-    print(f"The location you chose is {location_zip_json['name']}")
-    print(f"this weather today will be {cur_temp} degrees farienheight and it feels like {cur_feels_like}")
+    
+    print(f"The location you chose is {cur_weather_json['name']}")
+    print(f"The weather today will be a high of {cur_temp_max} degrees farienheight and it currently feels like {cur_feels_like}")
+    print(f'with {cur_sky}')
 
 def city_name_func():
     location_name = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},US&appid={API_KEY}')
@@ -28,20 +29,18 @@ def city_name_func():
    
     longitude = location_name_json[0]['lon']
     latitude = location_name_json[0]['lat']
-    
-    
 
     cur_weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=imperial')
     cur_weather_json = cur_weather.json()
-    print(cur_weather_json)
-    cur_temp = cur_weather_json['main']['temp']
+
     cur_feels_like = cur_weather_json['main']['feels_like']
     cur_temp_max = cur_weather_json['main']['temp_max']
-    cur_temp_min = cur_weather_json['main']['temp_min']
+    cur_sky = cur_weather_json['weather'][0]['description']
+
 
     print(f"The location you chose is {cur_weather_json['name']}")
-    print(f"The weather today will be a high of {cur_temp_max} degrees farienheight and a low of {cur_temp_min}")
-
+    print(f"The weather today will be a high of {cur_temp_max} degrees farienheight and it currently feels like {cur_feels_like}")
+    print(f'with {cur_sky}')
 
 
 mode = input("type \"zip\" to search by zip code or type \"name\" to seach by city name: ")
